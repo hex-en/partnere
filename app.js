b791eff80d55c97cf1f2cfa3e41c4d382fd0a8e0
Aleks Pawlak <aleks.pawlak@code.berlin>
2022-11-16 13:24:06 +0100

    third commit

--- app.js
+++ app.js
@@ -6,16 +6,21 @@ const PORT = 3000
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use('/assets', express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 
 app.get('/', (request, response) => {
     response.render('home')
 })
 app.get('/contact', (request, response) => {
-    response.send('contact')
+    response.render('contact')
+})
+app.post('/contact', (request, response) => {
+    console.log('Contact form submission: ', request.body)
+    response.send('Thank you for your message. We will be in touch soon.')
 })
 app.get('/login', (request, response) => {
-    response.send('login')
+    response.render('login')
 })
 
 app.listen(PORT, () => {
